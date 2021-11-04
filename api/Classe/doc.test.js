09dd3a7e5ee154fb8d43bfcc8f2af2165db62ecf
dy koffi <dykoffi2000@gmail.com>
2021-11-04 01:21:38 +0000

    modify test

--- api/Classe/doc.test.js
+++ api/Classe/doc.test.js
@@ -1,7 +1,7 @@
 const frisby = require('frisby')
 
 describe('GET /user', function() {
-    it("should return valid users list", function () {
+    it("should return l valid users list", function () {
         return frisby
         .get("http://localhost:3983/User")
         .expect("status", 403);
